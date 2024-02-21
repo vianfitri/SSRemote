edf43c3be33d9b840e8bf22f100f5ea9f16f54a1
vianfitri <viancukiep@gmail.com>
2024-02-21 13:33:33 +0700

    add reading button function

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 
-#define bt1 1
+#define bt1 A0
 #define bt2 2
 #define bt3 3
 #define bt4 4
@@ -18,6 +18,8 @@
 const byte slaveAddress[5] = {'S','s','T','M','U'};
 
 uint8_t stateBtn = 0x0;
+
+void ButtonReading();
 
 void setup() {
   Serial.begin(9600);
@@ -35,8 +37,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //stateBtn = stateBtn & (!digitalRead(bt1) << 0);
-  stateBtn = stateBtn & (1 << 0);
+  
+  // populate button
+  ButtonReading();
 
   Serial.println(stateBtn);
+}
+
+void ResetButton(){
+  stateBtn = 0x0;
+}
+
+void ButtonReading(){
+  // Reset Button
+  ResetButton();
+
+  // read button state
+  stateBtn |= (!digitalRead(bt1) << 0);
+  stateBtn |= (!digitalRead(bt2) << 1);
+  stateBtn |= (!digitalRead(bt3) << 2);
+  stateBtn |= (!digitalRead(bt4) << 3);
+  stateBtn |= (!digitalRead(bt5) << 4);
+  stateBtn |= (!digitalRead(bt6) << 5);
+  stateBtn |= (!digitalRead(bt7) << 6);
+  stateBtn |= (!digitalRead(bt8) << 7);
 }
